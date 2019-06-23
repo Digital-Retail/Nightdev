60dd5c8f69adc0b5baf4384b2d43fe12b90bbb34
Kirill Pakhtusov <pakhtusovkirill@gmail.com>
2019-06-23 18:24:52 +0300

    fixed mobile login page

--- login/index.php
+++ login/index.php
@@ -7,7 +7,7 @@
 <link href='https://fonts.googleapis.com/css?family=Arimo' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Hind:300' rel='stylesheet' type='text/css'>
 <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
-  
+<meta name="viewport" content="width=device-width, initial-scale=1">
   
       <link rel="stylesheet" href="css/style.css">
 
